40d03cf0bcc1e55275d302ea0d82effec1fd4219
Bertrand Chauveau <110421330+bertrandchauveau@users.noreply.github.com>
2024-11-29 16:26:53 +0100

    Update cov.py

--- torchstain/torch/utils/cov.py
+++ torchstain/torch/utils/cov.py
@@ -4,6 +4,6 @@ def cov(x, device='cpu'):
     """
     https://en.wikipedia.org/wiki/Covariance_matrix
     """
-    E_x = x.mean(dim=1, device=device)
+    E_x = x.mean(dim=1).to(device)
     x = x - E_x[:, None]
     return torch.mm(x, x.T) / (x.size(1) - 1)
