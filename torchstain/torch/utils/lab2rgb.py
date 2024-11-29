66a1911bc126bd1f368f885415623a3711a19456
Bertrand Chauveau <110421330+bertrandchauveau@users.noreply.github.com>
2024-11-29 16:21:01 +0100

    Update lab2rgb.py

--- torchstain/torch/utils/lab2rgb.py
+++ torchstain/torch/utils/lab2rgb.py
@@ -1,6 +1,7 @@
 import torch
 from torchstain.torch.utils.rgb2lab import _rgb2xyz, _white
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 _rgb2xyz = _rgb2xyz.to(device)
 _white = _white.to(device)
 _xyz2rgb = torch.linalg.inv(_rgb2xyz)
