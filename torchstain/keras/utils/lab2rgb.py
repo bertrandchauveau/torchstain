5e3abbcb1a1021aa8ad5577e4a44fa03355e6d6e
Bertrand Chauveau <110421330+bertrandchauveau@users.noreply.github.com>
2024-12-26 18:30:52 +0100

    Update lab2rgb.py

--- torchstain/keras/utils/lab2rgb.py
+++ torchstain/keras/utils/lab2rgb.py
@@ -2,14 +2,14 @@ import numpy as np
 import keras
 from torchstain.keras.utils.rgb2lab import _rgb2xyz
 
-_xyz2rgb = np.linalg.inv(_rgb2xyz)
+_xyz2rgb = keras.ops.inv(_rgb2xyz)
 
 """
 Implementation is based on:
 https://github.com/scikit-image/scikit-image/blob/00177e14097237ef20ed3141ed454bc81b308f82/skimage/color/colorconv.py#L704
 """
 def lab2rgb(lab):
-    lab = lab.astype("float32")
+    lab = keras.ops.cast(lab, dtype="float32")
     # first rescale back from OpenCV format
     lab[..., 0] /= 2.55
     lab[..., 1] -= 128
@@ -21,19 +21,19 @@ def lab2rgb(lab):
     x = (a / 500.) + y
     z = y - (b / 200.)
 
-    out = np.stack([x, y, z], axis=-1)
+    out = keras.ops.stack([x, y, z], axis=-1)
 
     mask = out > 0.2068966
-    out[mask] = np.power(out[mask], 3.)
+    out[mask] = keras.ops.power(out[mask], 3.)
     out[~mask] = (out[~mask] - 16.0 / 116.) / 7.787
 
     # rescale to the reference white (illuminant)
-    out *= np.array((0.95047, 1., 1.08883), dtype=out.dtype)
+    out *= keras.ops.convert_to_tensor(np.array((0.95047, 1., 1.08883), dtype=out.dtype))
     
     # convert XYZ -> RGB color domain
     arr = out.copy()
-    arr = np.dot(arr, _xyz2rgb.T)
+    arr = keras.ops.dot(arr, _xyz2rgb.T)
     mask = arr > 0.0031308
-    arr[mask] = 1.055 * np.power(arr[mask], 1 / 2.4) - 0.055
+    arr[mask] = 1.055 * keras.ops.power(arr[mask], 1 / 2.4) - 0.055
     arr[~mask] *= 12.92
-    return np.clip(arr, 0, 1)
+    return keras.ops.clip(arr, 0, 1)
