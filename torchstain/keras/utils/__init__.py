8ad2441896cb2dbaac8c48c4e45162fde59896f3
Bertrand Chauveau <110421330+bertrandchauveau@users.noreply.github.com>
2024-12-27 10:45:26 +0100

    Update __init__.py

--- torchstain/keras/utils/__init__.py
+++ torchstain/keras/utils/__init__.py
@@ -2,3 +2,4 @@ from torchstain.keras.utils.rgb2lab import *
 from torchstain.keras.utils.lab2rgb import *
 from torchstain.keras.utils.split import *
 from torchstain.keras.utils.stats import *
+from torchstain.keras.utils.cov import *
