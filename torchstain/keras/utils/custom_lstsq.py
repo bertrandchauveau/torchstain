8f01f0fb25b321fa66722656e5a94cb735c8f339
Bertrand Chauveau <110421330+bertrandchauveau@users.noreply.github.com>
2024-12-28 18:19:45 +0100

    Update custom_lstsq.py

--- torchstain/keras/utils/custom_lstsq.py
+++ torchstain/keras/utils/custom_lstsq.py
@@ -6,7 +6,7 @@ def custom_lstsq(A, B):
     Q, R = keras.ops.qr(A)  # A = Q @ R
 
     # Compute Q^T @ B (project B onto the subspace spanned by A's columns)
-    QTB = Q.T @ B
+    QTB = keras.ops.transpose(Q) @ B
 
     # Solve R @ X = Q^T @ B using torch.linalg.solve
     X = keras.ops.solve(R, QTB)
