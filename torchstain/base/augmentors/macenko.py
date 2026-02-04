c8d9c31394aafd37469e13de62cbd6d583ffa6b4
Bertrand Chauveau <110421330+bertrandchauveau@users.noreply.github.com>
2026-02-04 14:31:24 +0100

    Add support for Cupy backend in MacenkoAugmentor

--- torchstain/base/augmentors/macenko.py
+++ torchstain/base/augmentors/macenko.py
@@ -2,6 +2,9 @@ def MacenkoAugmentor(backend='torch', sigma1=0.2, sigma2=0.2):
     if backend == 'numpy':
         from torchstain.numpy.augmentors import NumpyMacenkoAugmentor
         return NumpyMacenkoAugmentor(sigma1=sigma1, sigma2=sigma2)
+    elif backend == 'cupy':
+        from torchstain.cupy.augmentors import CupyMacenkoAugmentor
+        return CupyMacenkoAugmentor(sigma1=sigma1, sigma2=sigma2)        
     elif backend == "torch":
         from torchstain.torch.augmentors import TorchMacenkoAugmentor
         return TorchMacenkoAugmentor(sigma1=sigma1, sigma2=sigma2)
